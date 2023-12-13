9a28404ea9d5476865b875f83d5d77bc5bda9128
bhawanaprasain <bhawana@fusemachines.com>
2023-12-13 10:17:29 +0545

    Load model for inference

---
+++ src/inference.py
@@ -0,0 +1,33 @@
+import torch
+import warnings
+from loguru import logger
+from transformers import BitsAndBytesConfig, AutoModelForCausalLM, AutoTokenizer
+from config import *
+
+class Chatbot:
+    def __init__(self, model_id):
+        self.model = None
+        self.tokenizer = None
+        self.device = "cuda"
+        self.initialize_chatbot(model_id)
+
+    def initialize_chatbot(self, model_id):
+        # Load the language model
+        quantization_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_compute_dtype=torch.float16,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_use_double_quant=True,
+        )
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_id,
+            quantization_config=quantization_config,
+            cache_dir=MISTRAL_MODEL_PATH,
+            use_auth_token=Env.api_token,
+        )
+
+        # Load the tokenizer
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model_id, padding_side="left", use_auth_token=Env.api_token, cache_dir=MISTRAL_TOKENIZER_PATH,
+
+        )
